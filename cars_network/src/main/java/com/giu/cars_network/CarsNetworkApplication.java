6065f87f8483f0800fee750676bd910ee79c7804
caligiure <caligiure74@gmail.com>
2026-04-18 17:30:18 +0200

    Spring Initializr: Java 17, Maven, Spring Boot 4.0.5

---
+++ cars_network/src/main/java/com/giu/cars_network/CarsNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.giu.cars_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsNetworkApplication.class, args);
+	}
+
+}
---
+++ cars_network/src/test/java/com/giu/cars_network/CarsNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giu.cars_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
